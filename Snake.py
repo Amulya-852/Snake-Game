1d2f683efeb35df27d2d0412a99d573b8223db53
Amulya <95533897+Amulya-852@users.noreply.github.com>
2022-09-26 21:42:51 +0530

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -1,105 +1,169 @@
-from tkinter import *
-import random,time
+#create snake game in python
+import turtle
+import random
+import time
 
-class Snake(Tk):
-    def __init__(self,*arge,**kwargs):
-        Tk.__init__(self,*arge,**kwargs)
-        self.initialSetup()
-        
-    def initialSetup(self):
-        self.base=Canvas(self,width=500,height=500)
-        self.base.pack(padx=10,pady=10)
-        self.snake=self.base.create_rectangle(1,1,21,21,fill="DodgerBlue2")
-        self.score=0
-        self.scoreDisplay=Label(self,text="Score:{}".format(self.score),font=('arial',20,'bold'))
-        self.scoreDisplay.pack(anchor='n')
-        self.length=3
-        self.target=None
-        self.gameStatus=1
-        self.x=20
-        self.y=0
-        self.bodycoords=[(0,0)]    
-        self.bind('<Any-KeyPress>',self.linkKeys)
-        return
 
-    def check_snake_coords(self):
-        self.base.move(self.snake,self.x,self.y)
-        i,j,ii,jj=self.base.coords(self.snake)
-        if i<=0 or j<=0 or ii>=500 or jj>=500:
-            self.x=0
-            self.y=0
-            #gameover
-            self.base.create_text(220,220,text="GAME OVER",font=('arial',40,'bold'),fill='red')
-            self.gameStatus=0
-        return
+#creating turtle screen
+screen = turtle.Screen()
+screen.title('Snake Game')
+screen.setup(600,600, startx=0,starty=100)
+screen.tracer(0)
+turtle.bgcolor('turquoise')
 
-    def move_snake(self):
-        i,j,ii,jj=self.base.coords(self.snake)
-        ii=(ii-((ii-i)/2))
-        jj=(jj-((jj-j)/2))
-        self.bodycoords.append((ii,jj))
-        self.base.delete('snakebody')
-        if len(self.bodycoords)>=self.length:
-            self.bodycoords=self.bodycoords[-self.length:]
-        self.base.create_line(tuple(self.bodycoords),tag='snakebody',width=20,fill="DodgerBlue2")
-        return
-        
-    def food(self):
-        if self.target==None:
-            a=random.randint(20,480)
-            b=random.randint(20,480)
-            self.target=self.base.create_oval(a,b,a+20,b+20,fill='red',tag='food')
-            #print(self.base.coords(self.target))
-        if self.target:
-            #print(self.base.coords(self.target))
-            i,j,ii,jj=self.base.coords(self.target)
-            #time.sleep(0.1)
-            if len(self.base.find_overlapping(i,j,ii,jj))!=1:
-                self.base.delete("food")
-                self.target=None
-                self.updateScore()
-                self.length+=1
-            return
 
-    def updateScore(self):
-        self.score+=1
-        self.scoreDisplay['text']="Score : {}".format(self.score)
-        return
 
-    def linkKeys(self,event=None):
-        pressedkey=event.keysym
-        if pressedkey=='Left':
-            self.x=-20
-            self.y=0
+##creating a border for our game
 
-        elif pressedkey=='Up':
-            self.x=0
-            self.y=-20
+turtle.speed(2)
+turtle.pensize(4)
+turtle.penup()
+turtle.goto(-280,250)
+turtle.pendown()
+turtle.color('black')
+turtle.forward(500)
+turtle.right(90)
+turtle.forward(500)
+turtle.right(90)
+turtle.forward(500)
+turtle.right(90)
+turtle.forward(500)
+turtle.penup()
+turtle.hideturtle()
 
-        elif pressedkey=='Right':
-            self.x=20
-            self.y=0
+#score
+score = 0
+delay = 0.1
 
-        elif pressedkey=='Down':
-            self.x=0
-            self.y=20
 
-        else:
-            pass
-        return
+#snake
+snake = turtle.Turtle()
+snake.speed(0)
+snake.shape('square')
+snake.color("black")
+snake.penup()
+snake.goto(0,0)
+snake.direction = 'stop'
 
-    def manage(self):
-        if(self.gameStatus==0):
-            return
-        self.check_snake_coords()
-        self.move_snake()
-        self.food()
-        
-        return
 
-snakeobj=Snake(className="ProjectGurukul Snake Game")
+#food
+fruit = turtle.Turtle()
+fruit.speed(0)
+fruit.shape('circle')
+fruit.color('red')
+fruit.penup()
+fruit.goto(30,30)
+
+old_fruit=[]
+
+#scoring
+scoring = turtle.Turtle()
+scoring.speed(0)
+scoring.color("black")
+scoring.penup()
+scoring.hideturtle()
+scoring.goto(0,300)
+scoring.write("Score :",align="center",font=("Courier",24,"bold"))
+
+
+#######define how to move
+def snake_go_up():
+    if snake.direction != "down":
+        snake.direction = "up"
+
+def snake_go_down():
+    if snake.direction != "up":
+        snake.direction = "down"
+
+def snake_go_left():
+    if snake.direction != "right":
+        snake.direction = "left"
+
+def snake_go_right():
+    if snake.direction != "left":
+        snake.direction = "right"
+
+def snake_move():
+    if snake.direction == "up":
+        y = snake.ycor()
+        snake.sety(y + 20)
+
+    if snake.direction == "down":
+        y = snake.ycor()
+        snake.sety(y - 20)
+
+    if snake.direction == "left":
+        x = snake.xcor()
+        snake.setx(x - 20)
+
+    if snake.direction == "right":
+        x = snake.xcor()
+        snake.setx(x + 20)
+
+# Keyboard bindings
+screen.listen()
+screen.onkeypress(snake_go_up, "Up")
+screen.onkeypress(snake_go_down, "Down")
+screen.onkeypress(snake_go_left, "Left")
+screen.onkeypress(snake_go_right, "Right")
+
+#main loop
+
 while True:
-    snakeobj.update()
-    snakeobj.update_idletasks()
-    snakeobj.manage()
-    time.sleep(0.4)
+        screen.update()
+            #snake and fruit coliisions
+        if snake.distance(fruit)< 20:
+                x = random.randint(-290,270)
+                y = random.randint(-240,240)
+                fruit.goto(x,y)
+                scoring.clear()
+                score+=1
+                scoring.write("Score:{}".format(score),align="center",font=("Courier",24,"bold"))
+                delay-=0.001
+                
+                ## creating new_ball
+                new_fruit = turtle.Turtle()
+                new_fruit.speed(0)
+                new_fruit.shape('square')
+                new_fruit.color('red')
+                new_fruit.penup()
+                old_fruit.append(new_fruit)
+                
+
+        #adding ball to snake
+        
+        for index in range(len(old_fruit)-1,0,-1):
+                a = old_fruit[index-1].xcor()
+                b = old_fruit[index-1].ycor()
+
+                old_fruit[index].goto(a,b)
+                                     
+        if len(old_fruit)>0:
+                a= snake.xcor()
+                b = snake.ycor()
+                old_fruit[0].goto(a,b)
+        snake_move()
+
+        ##snake and border collision    
+        if snake.xcor()>280 or snake.xcor()< -300 or snake.ycor()>240 or snake.ycor()<-240:
+                time.sleep(1)
+                screen.clear()
+                screen.bgcolor('turquoise')
+                scoring.goto(0,0)
+                scoring.write("   GAME OVER \n Your Score is {}".format(score),align="center",font=("Courier",30,"bold"))
+
+
+        ## snake collision
+        for food in old_fruit:
+                if food.distance(snake) < 20:
+                        time.sleep(1)
+                        screen.clear()
+                        screen.bgcolor('turquoise')
+                        scoring.goto(0,0)
+                        scoring.write("    GAME OVER \n Your Score is {}".format(score),align="center",font=("Courier",30,"bold"))
+
+
+                
+        time.sleep(delay)
+
+turtle.Terminator()
